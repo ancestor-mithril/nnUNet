691b8459db810422df49a2d907416e15c2433a48
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2026-03-13 13:36:13 +0200

    WIP: Inference script update dockerfile

--- nnunetv2/inference_service/create_docker.py
+++ nnunetv2/inference_service/create_docker.py
@@ -69,7 +69,10 @@ ENV nnUNet_preprocessed=/app/nnUNet_preprocessed
 WORKDIR /app
 
 RUN mkdir -p /app/input /app/output /app/nnUNet_raw /app/nnUNet_results /app/nnUNet_preprocessed && \
-    pip install timed-decorator && pip uninstall nnunetv2 --yes && pip install 'git+https://github.com/ancestor-mithril/nnUNet.git@new' --no-cache-dir
+    chmod 777 /app/output && \
+    pip install timed-decorator && \
+    pip uninstall nnunetv2 --yes && \
+    pip install 'git+https://github.com/ancestor-mithril/nnUNet.git@new' --no-cache-dir
 
 COPY checkpoint_final.pth /app/nnUNet_results/{docker_model_folder}/fold_{args.f}/
 COPY dataset.json plans.json /app/nnUNet_results/{docker_model_folder}/
