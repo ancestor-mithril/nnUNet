81e28bcb7288574282dcb554191291e8ca0a866b
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2026-09-03 22:09:52 +0300

    using label metrics

--- nnunetv2/docker_service/create_docker.py
+++ nnunetv2/docker_service/create_docker.py
@@ -50,7 +50,7 @@ RUN mkdir -p /app/input /app/output ${{cont_data_path}} ${{cont_model_path}} ${{
     chmod 777 /home/jovyan && \
     chmod 777 ${{cont_preproc_path}} && \
     chmod 777 ${{cont_model_path}} && \
-    pip install timed-decorator dice-score-3d && \
+    pip install timed-decorator dice-score-3d 'git+https://github.com/fii-optim-lab/label-metrics.git' && \
     pip uninstall nnunetv2 --yes && \
     pip install 'git+https://github.com/ancestor-mithril/nnUNet.git@new' --no-cache-dir && \
     COMMIT_HASH=$(git ls-remote "https://github.com/ancestor-mithril/nnUNet.git" "refs/heads/new" | awk '{{print $1}}') && \
