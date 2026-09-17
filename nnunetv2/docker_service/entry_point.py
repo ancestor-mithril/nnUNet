19c52a30c871fec1ad3ac804962450a8fb9cdecb
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2026-09-17 08:30:18 +0300

    0.35 step size

--- nnunetv2/docker_service/entry_point.py
+++ nnunetv2/docker_service/entry_point.py
@@ -385,7 +385,7 @@ def validate(args):
     if not os.path.isfile(model_checkpoint):
         raise FileNotFoundError(f"Model checkpoint {model_checkpoint} not available, please train the model first")
 
-    for step_size in [0.25, 0.5]:
+    for step_size in [0.35, 0.5]:
         if step_size != 0.5:
             validation_path = os.path.join(fold_path, f"validation_{step_size}")
         else:
